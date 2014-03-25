c7c2d01f79768edc7201982090f57bc2f3ae35b4
shuhei1211 <unus6812>
2014-03-26 01:19:24 +0900

    butterknife and micheranjero sample1

--- ButterknifeTest/app/src/main/java/jp/butterknifetest/app/MainActivity.java
+++ ButterknifeTest/app/src/main/java/jp/butterknifetest/app/MainActivity.java
@@ -6,6 +6,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.romainpiel.michelangelo.InflateLayout;
+import com.romainpiel.michelangelo.InjectViews;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,7 +34,20 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
+    @InflateLayout(R.layout.fragment_main)
+    @InjectViews
     public static class PlaceholderFragment extends Fragment {
+
+        @InjectView(R.id.text)
+        TextView mTextView;
+
+        @InjectView(R.id.button)
+        Button mButton;
+
+        @OnClick(R.id.button)
+        public void onClick() {
+            mTextView.setText("butter knife test");
+        }
 
         public PlaceholderFragment() {
         }
@@ -34,7 +56,16 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // initiarize ButterKnife
+            ButterKnife.inject(this, rootView);
             return rootView;
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            ButterKnife.reset(this);
         }
     }
 }
